041d04aba25df7db015b3ad22a4f306358ac951e
Koda Passey <kodapassey@charter.net>
2023-06-28 14:14:30 -0700

    fixed error with port in index.js

--- index.js
+++ index.js
@@ -135,6 +135,8 @@ app.delete('/eaglepoint/:id', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Listening on port ' + 3000);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
